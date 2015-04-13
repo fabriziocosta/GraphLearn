faf8b0718ccd5049a05c6ac2196d9fa3f9c055b0
Fabrizio Costa <xfcosta@gmail.com>
2015-04-13 16:42:51 +0200

    Initial code population

---
+++ utils/draw.py
@@ -0,0 +1,197 @@
+
+import networkx as nx
+import pylab as plt
+from eden.util.display  import draw_graph
+import itertools
+from myeden import *
+'''
+        
+    functions to draw graphs:
+        -draw_grammar
+            visialize the samplers grammar
+        -display(graph)
+            use edens drawing thing, but make sure cores and interfaces are marked
+        -drawgraphs(list)
+            drawing a list of graphs
+'''
+
+def display( G,size=15,font_size=15,node_size=200,node_border=False,delabeledges=True,contract=False,vertex_label='label'):
+    
+    if contract:
+        G=contract_edges(G)
+
+    G2=G.copy()
+    for n,d in G2.nodes(data=True):
+        if 'core' in d:
+            d['color']='red'
+        elif 'interface' in d:
+            d['color']='yellow'
+        else:
+            d['color']='green' 
+    if delabeledges:
+        for a,b,c in G2.edges_iter(data=True):
+            c['label']=''       
+
+    draw_graph(G2, size=size, node_size=node_size, node_border=node_border,font_size=font_size,vertex_color='color',vertex_label=vertex_label)
+
+
+def draw_grammar(sampler,interfacecount):
+    
+    
+    grammar=sampler.substitute_grammar
+    #how many rows to draw...
+    if len(grammar) < interfacecount:
+        interfacecount=len(grammar)
+    
+    for i in range(interfacecount):
+        interface=grammar.keys()[i]
+        
+        core_cid_dict=grammar[interface]
+        
+        graphs=[  core_cid_dict[chash].graph for i, chash in enumerate(core_cid_dict.keys()) if i < 5  ]
+        
+        print 'interface: '+str(interface)
+        drawgraphs(graphs,len(core_cid_dict))
+
+
+    
+def drawgraphs(graphs,contract=True,deleteedges=True,size=4):
+    count= len(graphs)
+    size_y = size
+    size_x = size_y*count
+    plt.figure( figsize = ( size_x,size_y ) )
+    plt.xlim(xmax=3)
+
+    for x in range(count):
+        
+        plt.subplot(1,count,x+1)
+        row_drawgraph_wrapper(graphs[x],contract=contract,deleteedges=deleteedges)
+    
+    plt.show()
+        
+
+def row_drawgraph_wrapper( G,size=15,font_size=15,node_size=200,node_border=False,contract=True,deleteedges=True):
+    
+   
+    if contract:
+        G=contract_edges(G)
+
+    if deleteedges:
+        for a,b,c in G.edges_iter(data=True):
+            c['label']=''
+            
+    for a,b,c in G.edges_iter(data=True):
+        if 'label' not in c:
+            c['label']=''    
+        
+    G2=G.copy()
+    for n,d in G2.nodes(data=True):
+        if 'core' in d:
+            d['color']='blue'
+        elif 'interface' in d:
+            d['color']='pink'
+        else:
+            d['color']='yellow' 
+        
+    row_draw_graph(G2, size=size, node_size=node_size, node_border=node_border,font_size=font_size,vertex_color='color')
+
+
+
+
+def row_draw_graph( graph, 
+    vertex_label = 'label', 
+    secondary_vertex_label = None, 
+    edge_label = 'label', 
+    secondary_edge_label = None, 
+    vertex_color = '', 
+    vertex_alpha = 0.6,
+    size = 10,
+    size_x_to_y_ratio = 1, 
+    node_size = 600,
+    font_size = 9,
+    layout = 'graphviz', 
+    prog  =  'neato',
+    node_border = False,
+    colormap = 'YlOrRd',
+    invert_colormap = False,
+    verbose = True ):
+    '''
+        thisis basically taken from eden,
+        but calling figure() and show() are disables
+        so i can draw many graphs in a row
+    '''
+
+    size_x = size
+    size_y = int(float(size) / size_x_to_y_ratio)
+
+    #plt.figure( figsize = ( size_x,size_y ) )
+    plt.grid( False )
+    plt.axis( 'off' )
+    
+    if secondary_vertex_label:
+        vertex_labels = dict( [( u,'%s\n%s'%( d.get( vertex_label,'N/A' ),d.get( secondary_vertex_label,'N/A' )  )  ) for u,d in graph.nodes( data = True )] )
+    else:
+        vertex_labels = dict( [( u,d.get( vertex_label,'N/A' ) ) for u,d in graph.nodes( data = True ) ] )
+    
+    edges_normal = [( u,v ) for ( u,v,d ) in graph.edges( data = True ) if d.get( 'nesting', False ) == False]
+    edges_nesting = [( u,v ) for ( u,v,d ) in graph.edges( data = True ) if d.get( 'nesting', False ) == True]
+
+    if secondary_edge_label:
+        edge_labels = dict( [( ( u,v, ),'%s\n%s'%( d.get( edge_label,'N/A' ),d.get( secondary_edge_label,'N/A' ) )  ) for u,v,d in graph.edges( data = True )] )
+    else:
+        edge_labels = dict( [( ( u,v, ),d.get( edge_label,'N/A' )  ) for u,v,d in graph.edges( data = True )] )
+
+    if vertex_color == '':
+        node_color  =  'white'
+    else:
+        if invert_colormap:
+            node_color = [ - d.get( vertex_color,0 ) for u,d in graph.nodes( data = True ) ]
+        else:
+            node_color = [ d.get( vertex_color,0 ) for u,d in graph.nodes( data = True ) ]
+
+    if layout == 'graphviz':
+        pos  =  nx.graphviz_layout( graph, prog  =  prog )
+    elif layout == 'circular':
+        pos  =  nx.circular_layout( graph )
+    elif layout == 'random':
+        pos  =  nx.random_layout( graph )
+    elif layout == 'spring':
+        pos  =  nx.spring_layout( graph )
+    elif layout == 'shell':
+        pos  =  nx.shell_layout( graph )
+    elif layout == 'spectral':
+        pos  =  nx.spectral_layout( graph )
+    else:
+        raise Exception( 'Unknown layout format: %s' % layout )
+
+    if node_border == False :
+        linewidths  =  0.001
+    else:
+        linewidths  =  1
+
+    nx.draw_networkx_nodes( graph,pos,
+        node_color  =  node_color,
+        alpha  =  vertex_alpha,
+        node_size  =  node_size, 
+        linewidths  =  linewidths,
+        cmap  =  plt.get_cmap( colormap ) )
+    nx.draw_networkx_labels( graph,pos, vertex_labels, font_size  =  font_size,font_color  =  'black' )
+    nx.draw_networkx_edges( graph, pos, 
+        edgelist  =  edges_normal, 
+        width  =  2, 
+        edge_color  =  'k', 
+        alpha  =  0.5 )
+    nx.draw_networkx_edges( graph, pos, 
+        edgelist  =  edges_nesting, 
+        width  =  1, 
+        edge_color  =  'k', 
+        style  =  'dashed', 
+        alpha  =  0.5 )
+    nx.draw_networkx_edge_labels( graph, pos, edge_labels  =  edge_labels, font_size  =  font_size )
+    if verbose:
+        title = str(graph.graph.get('id','')) + "\n" + str(graph.graph.get('info',''))
+        plt.title(title)
+    #plt.show()
+
+
+
---
+++ utils/myeden.py
@@ -0,0 +1,103 @@
+import networkx as nx
+from eden.graph import Vectorizer
+import numpy as np
+from scipy.sparse import vstack
+from sklearn.linear_model import SGDClassifier
+from sklearn.grid_search import RandomizedSearchCV
+from scipy.stats import randint
+from scipy.stats import uniform
+
+'''
+    wrapped or altered eden functions:
+        -my_vectorizer, a eden vectorizer that doesnt try to expand graphs
+        -my_fit_estimator, a estimator that doesnt do validation and doesnt print 
+        -expand_edges, just for convenience.
+        
+'''
+
+
+class my_vectorizer(Vectorizer):
+    '''
+    doing some overwriting so we dont expand and contract edges all the time..
+    this hack is a little bit dependant on the state of eden.. so be carefull here 
+    '''
+    def _edge_to_vertex_transform(self,graph):
+        return nx.Graph(graph)
+
+    def transform2(self,graph): 
+        return self._convert_dict_to_sparse_matrix(self._transform(0, graph))
+
+def my_fit_estimator(positive_data_matrix=None, negative_data_matrix=None, target=None, cv=10, n_jobs=-1):
+    '''
+     we dont need the validation at all... so i just copied from eden/utils/__init__.py
+    '''
+    assert(
+        positive_data_matrix is not None), 'ERROR: expecting non null positive_data_matrix'
+    if target is None and negative_data_matrix is not None:
+        yp = [1] * positive_data_matrix.shape[0]
+        yn = [-1] * negative_data_matrix.shape[0]
+        y = np.array(yp + yn)
+        X = vstack([positive_data_matrix, negative_data_matrix], format="csr")
+    if target is not None:
+        X = positive_data_matrix
+        y = target
+
+    predictor = SGDClassifier(class_weight='auto', shuffle=True, n_jobs=n_jobs)
+    # hyperparameter optimization
+    param_dist = {"n_iter": randint(5, 100),
+                  "power_t": uniform(0.1),
+                  "alpha": uniform(1e-08, 1e-03),
+                  "eta0": uniform(1e-03, 10),
+                  "penalty": ["l1", "l2", "elasticnet"],
+                  "learning_rate": ["invscaling", "constant", "optimal"]}
+    scoring = 'roc_auc'
+    n_iter_search = 20
+    random_search = RandomizedSearchCV(
+        predictor, param_distributions=param_dist, n_iter=n_iter_search, cv=cv, scoring=scoring, n_jobs=n_jobs)
+    random_search.fit(X, y)
+    optpredictor = SGDClassifier(
+        class_weight='auto', shuffle=True, n_jobs=n_jobs, **random_search.best_params_)
+    # fit the predictor on all available data
+    optpredictor.fit(X, y)
+    return optpredictor
+
+
+def expand_edges(graph):
+    '''
+    convenience wrapper
+    '''
+    vectorizer= Vectorizer(r=2,d=2) 
+    return vectorizer._edge_to_vertex_transform(graph)
+            
+            
+
+
+
+def contract_edges( original_graph):
+    """
+        stealing from eden...
+        because i draw cores and interfaces there may be edge-nodes 
+        that have no partner, eden gives error in this case.
+        i still want to see them :) 
+    """
+    # start from a copy of the original graph
+    G = nx.Graph(original_graph)
+    # re-wire the endpoints of edge-vertices
+    for n, d in original_graph.nodes_iter(data=True):
+        if d.get('edge', False) == True:
+            # extract the endpoints
+            endpoints = [u for u in original_graph.neighbors(n)]
+            #assert (len(endpoints) == 2), 'ERROR: more than 2 endpoints'
+            if len(endpoints)!=2:
+                continue
+            u = endpoints[0]
+            v = endpoints[1]
+            # add the corresponding edge
+            G.add_edge(u, v, d)
+            # remove the edge-vertex
+            G.remove_node(n)
+        if d.get('node', False) == True:
+            # remove stale information
+            G.node[n].pop('remote_neighbours', None)
+    return G
+
