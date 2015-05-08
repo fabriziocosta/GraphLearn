3c268bd60aa5e9340baf20b9d2392140c5df9ece
smautner <ikeautf@web.de>
2015-05-09 01:34:33 +0200

    cluster stuff ready to test

---
+++ example/cluster.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append('..')
+import os
+os.nice(19)
+%matplotlib inline
+
+import graphlearn.utils.draw as myutils
+import graphlearn.cluster as cl
+from eden.converter.graph.gspan import gspan_to_eden
+import itertools
+import matplotlib.pyplot as plt
+
+
+steps=100
+
+sampler=cl.cluster()
+
+cluster.fit(gspan_to_eden( 'bursi.pos.gspan' ))
+cluster.save('tmp/cluster.ge')
+
+#sampler.load('tmp/cluster.ge')
+
+
+
+graphs = gspan_to_eden( 'bursi.pos.gspan' )
+graphs = itertools.islice(graphs,9)
+graphs = sampler.sample(graphs,
+                        sampling_interval=int(steps/3)+1,
+                        batch_size=1,
+                        n_steps=steps,
+                        n_jobs=1,
+                        )
+
+history=[]
+for  i, (result,info) in enumerate(graphs):
+    history.append(info['score_history'])
+    myutils.draw_many_graphs(info['graphs'])
+
+t = range(steps+1) 
+for h in history[:3]:
+    plt.plot(t, h)
+plt.show()
+t = range(steps+1) 
+for h in history[3:6]:
+    plt.plot(t, h)
+plt.show()
+t = range(steps+1) 
+for h in history[6:]:
+    plt.plot(t, h)
+plt.show()
+
