dc69fdfaccc3315ae34ba99273e1f8b8b9f35d5d
smautner <qxc420@gmail.com>
2015-06-10 15:42:15 +0200

    found bug.. not sure what to do with that,,

--- graphlearn/discsampler.py
+++ graphlearn/discsampler.py
@@ -1,17 +1,18 @@
-from graphlearn import GraphLearnSampler, LocalSubstitutableGraphGrammar
+from graphlearn import GraphLearnSampler
 import itertools
 import networkx as nx
-from scipy.sparse import csr_matrix, vstack
-from sklearn.neighbors import NearestNeighbors
+#from scipy.sparse import csr_matrix, vstack
+#from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import LSHForest
-import numpy as np
-import heapq
-from eden.util import fit
+#import numpy as np
+#import heapq
+#from eden.util import fit
 from eden.graph import Vectorizer
-import copy
+#import copy
 import heapq
 
-from utils.draw import draw_grammar
+from utils import draw
+#from utils.draw import draw_grammar
 class DiscSampler():
     '''
     '''
@@ -20,28 +21,48 @@ class DiscSampler():
         # this is mainly for the forest. the sampler uses a different vectorizer
         self.vectorizer = Vectorizer(nbits=14)
 
-    def get_heap_and_forest(self, iter, k):
+    def get_heap_and_forest(self, griter, k):
         '''
         so we create the heap and the forest...
         heap is (dist to hyperplane, count, graph)
         and the forest ist just a nearest neighbor from sklearn
         '''
-        iter, iter2 = itertools.tee(iter)
-        X = self.vectorizer.transform(iter)
-        forest = LSHForest()
-        forest.fit(X)
-        heap = []
-        print 'got forest'
-        for vector, graph in itertools.izip(X, iter2):
-            graph2 = nx.Graph(graph)
-            heapq.heappush(heap, (
-            self.sampler.estimator.predict_proba(self.sampler.vectorizer.transform_single(graph2))[0][1], k + 1, graph))
-        print 'got heap'
-        distances, unused = forest.kneighbors(X, n_neighbors=2)
-        distances = [a[1] for a in distances]  # the second element should be the dist we want
-        avg_dist = distances[len(distances) / 2]  # sum(distances)/len(distances)
-        print 'got dist'
-        return heap, forest, avg_dist
+        griter, iter2 = itertools.tee(griter)
+        #~ X = self.vectorizer.transform(griter)
+        
+        #~ forest = LSHForest()
+        #~ forest.fit(X)
+        #~ print 'got forest'
+        
+        #~ heap = []
+        #~ for vector, graph in itertools.izip(X, iter2):
+            #~ graph2 = nx.Graph(graph)
+            #~ heapq.heappush(heap, (
+            #~ self.sampler.estimator.predict_proba(self.sampler.vectorizer.transform_single(graph2))[0][1], k + 1, graph))
+        #~ print 'got heap'
+        #~ distances, unused = forest.kneighbors(X, n_neighbors=2)
+        #~ distances = [a[1] for a in distances]  # the second element should be the dist we want
+        #~ avg_dist = distances[len(distances) / 2]  # sum(distances)/len(distances)
+        #~ print 'got dist'
+        #~ 
+        #~ return heap, forest, avg_dist
+        #~ 
+        return 1,2,3
+    def sample_simple(self,graphiter,iterneg):
+        graphiter,grait,griter2 = itertools.tee(graphiter,3)
+        
+        self.fit_sampler(graphiter,iterneg)
+        a,b,c=self.get_heap_and_forest( griter2, 30)
+        
+        rez=self.sampler.sample(grait,sampling_interval=5,
+                                       batch_size=1,
+                                       n_jobs=0,
+                                       n_steps=30,
+                                       select_cip_max_tries=100,
+                                       accept_annealing_factor=.5,
+                                       generatormode=False,
+                                       same_core_size=False )
+        return rez
 
     def sample_graphs(self, graphiter, iter_neg, radius, how_many, check_k, heap_chunk_size=10):
 
@@ -50,10 +71,10 @@ class DiscSampler():
         # setup heap and forest
         graphiter, iter2 = itertools.tee(graphiter)
         self.fit_sampler(iter2, iter_neg)
+
         heap, forest, avg_dist = self.get_heap_and_forest(graphiter, check_k)
         # heap should be like   (hpdist, count, graph)
         radius = radius * avg_dist
-
         # so lets start the loop1ng
         result = []
         while heap and len(result) < how_many:
@@ -66,16 +87,17 @@ class DiscSampler():
 
             # let the sampler do the sampling
             graphz = [e[2] for e in todo]
+            #draw.draw_graph_set_graphlearn(graphz)
             work = self.sampler.sample(graphz,
-                                       batch_size=3,
-                                       n_jobs=1,
+                                       batch_size=1,
+                                       n_jobs=0,
                                        n_steps=30,
                                        select_cip_max_tries=100,
                                        accept_annealing_factor=.5,
                                        generatormode=False,
-                                       same_core_size=False 
+                                       same_core_size=False,
+                                       sampling_interval=3
                                        )
-
             # lets see, we need to take care of
             # = the initialy poped stuff
             # - increase and check the counter, reinsert into heap
@@ -84,16 +106,16 @@ class DiscSampler():
             for graph, task in zip(work, todo):
                 graphlist = graph.graph['sampling_info']['graphs_history']
                 print 'rez:', graphlist, task
-                for graph in graphlist:
+                for graph2 in graphlist:
                     # check distance from created instances
-                    x = self.vectorizer.transform_single(graph)
+                    x = self.vectorizer.transform_single(graph2)
                     dist, void = forest.kneighbors(x, 1)
                     dist = sum(dist)
                     # is the distance ok?
                     # if so, insert into forest and heap
                     if radius < dist < radius * 2:
                         forest.partial_fit(x)
-                        heapq.heappush(heap, (graph.graph['score'], 0, graph))
+                        heapq.heappush(heap, (graph2.graph['score'], 0, graph2))
                         print 'heap'
                     print 'cant heap', radius, dist
                 # taking care of task graph
@@ -104,18 +126,24 @@ class DiscSampler():
                 # go back to the heap!
                 heapq.heappush(heap, (task[0], task[1] + len(graphlist), task[2]))
 
+
+
         return result
 
-    def fit_sampler(self, iter_pos, iter_neg):
 
+    def simple_fit(self,iter_pos):
+        self.sampler= GraphLearnSampler()
+        self.sampler.fit(iter_pos)
+        self.estimator=self.sampler.estimator
+
+    def fit_sampler(self, iter_pos, iter_neg):
         # getting the sampler ready:
-        vectorizer= Vectorizer(complexity=3, nbits=20)
-        self.sampler = MySampler(vectorizer=vectorizer)
+        self.sampler = MySampler(complexity=3)
         iter_pos, pos, pos_ = itertools.tee(iter_pos, 3)
-        self.estimator = self.sampler.estimatorobject.fit_2(iter_pos, iter_neg, vectorizer)
+        self.estimator = self.sampler.estimatorobject.fit_2(iter_pos, iter_neg, self.sampler.vectorizer)
         print 'got estimeetaaa'
-        self.sampler.estimator = self.estimator
         self.sampler.fit_grammar(pos)
+        self.sampler.estimator = self.estimator
         print 'got grammar:grammar is there oO'
         #draw_grammar(self.sampler.local_substitutable_graph_grammar.grammar,n_productions=5)
         
@@ -134,7 +162,7 @@ class MySampler(GraphLearnSampler):
             self.sample_path.append(graph)
 
         if len(self.sample_path) > 3:
-            raise ('stop condition reached')
+            raise Exception('stop condition reached')
 
     def _sample_path_append(self, graph):
         pass
