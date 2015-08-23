8556547cc51a863ab2568bbae3b3a238d35c6cfe
smautner <qxc420@gmail.com>
2015-08-23 23:51:16 +0200

    cycle abstractor created

--- graphlearn/abstract_graphs/molecules.py
+++ graphlearn/abstract_graphs/molecules.py
@@ -3,6 +3,7 @@ from ubergraphlearn import UberSampler,UberGrammar
 import ubergraphlearn
 import networkx as nx
 import graphlearn.utils.draw as draw
+from collections import defaultdict
 
 
 
@@ -83,3 +84,164 @@ class GraphManager(object):
 
     def get_abstract_graph(self):
         return self.abstract_graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+here we invent the abstractor function
+'''
+import networkx as nx
+import graphlearn.utils.draw as draw
+
+
+def make_abstract(extgraph):
+    '''
+    ok the plan:
+    build abstract graph by
+        - get abstractnodes
+        - save abs parents in each node of base graph
+        - look at all the edge nodes in the base graph to connect the abs graph nodes
+    :param graph: an edge to vertex transformed graph
+    :return: edge to vertex transformed abstract graph
+    '''
+
+    pass
+
+
+
+
+def node_to_cycle(graph,n,min_cycle_size=3):
+    """
+    :param graph:
+    :param n: start node
+    :param min_cycle_size:
+    :return:  a cycle the node belongs to
+
+    so we start in node n,
+    then we expand 1 node further in each step.
+    if we meet a node we had before we found a cycle.
+
+    there are 3 possible cases.
+        - frontier hits frontier -> cycle of even length
+        - frontier hits visited nodes -> cycle of uneven length
+        - it is also possible that the newly found cycle doesnt contain our start node. so we check for that
+    """
+
+    def close_cycle(collisions,parent,root):
+        '''
+            we found a cycle, but that does not say that the root node is part of that cycle..
+
+        '''
+        def extend_path_to_root(work_list,parent_dict,root):
+            """
+            :param work_list: list with start node
+            :param parent_dict: the tree like dictionary that contains each nodes parent(s)
+            :param root: root node. probably we dont really need this since the root node is the orphan
+            :return: cyclenodes or none
+
+             --- mm we dont care if we get the shortest path.. that is true for cycle checking.. but may be a problem in
+             --- cycle finding.. dousurururururu?
+            """
+            current=work_list[-1]
+            while current != root:
+                work_list.append(  parent_dict[current][0]  )
+                current=work_list[-1]
+            return work_list[:-1]
+
+        # any should be fine. e is closing a cycle,
+        # note: e might contain more than one hit but we dont care
+        e= collisions.pop()
+        #print 'r',e
+        # we closed a cycle on e so e has 2 parents...
+        li=parent[e]
+        a = [li[0]]
+        b = [li[1]]
+        #print 'pre',a,b
+        # get the path until the root node
+        a=extend_path_to_root(a,parent,root)
+        b=extend_path_to_root(b,parent,root)
+        #print 'comp',a,b
+        #of the paths to the root node dont overlap, the root node musst be in the loop
+        a=set(a)
+        b=set(b)
+        intersect=a & b
+        if len(intersect) ==0:
+            paths= a | b
+            paths.add(e)
+            paths.add(root)
+            return  paths
+        return False
+
+
+    FAILEDVALUE = set([n])
+    frontier= set([n])
+    step=0
+    visited=set()
+    parent=defaultdict(list)
+
+    while frontier:
+        #print frontier
+        step+=1
+
+        # give me new nodes:
+        next=[]
+        for front_node in frontier:
+            new = set(graph.neighbors(front_node)) - visited
+            next.append(  new )
+            for e in new:
+                parent[e].append(front_node)
+
+        # we merge the new nodes.   if 2 sets collide, we found a cycle of even length
+        while len(next)>1:
+            # merge
+            s1=next[1]
+            s2=next[0]
+            merge = s1 | s2
+
+            # check if we havee a cycle   => s1,s2 overlap
+            if  len(merge) < len(s1)+len(s2):
+                col= s1&s2
+                cycle=close_cycle(col,parent,n)
+                if cycle:
+                    if step*2 > min_cycle_size:
+                        return cycle
+                    return FAILEDVALUE
+
+            #delete from list
+            next[0]=merge
+            del next[1]
+        next=next[0]
+
+        # now we need to check for cycles of uneven length => the new nodes hit the old frontier
+        if len(next & frontier) > 0:
+            col= next&frontier
+            cycle = close_cycle(col,parent,n)
+            if cycle:
+                if step*2-1 > min_cycle_size:
+                    return cycle
+                return FAILEDVALUE
+
+        # we know that the current frontier didntclose cycles so we dont need to look at them again
+        visited = visited | frontier
+        frontier=next
+    return FAILEDVALUE
+
+
+
+
+
+
+
+
+
