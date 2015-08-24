6919b6d8a9b69951a80e0203e8e59fcc3d164be0
smautner <qxc420@gmail.com>
2015-08-24 15:37:33 +0200

    abstract molecule sampling works

--- graphlearn/abstract_graphs/molecules.py
+++ graphlearn/abstract_graphs/molecules.py
@@ -9,8 +9,6 @@ from collections import defaultdict
 
 class MoleculeSampler(UberSampler):
 
-
-
     def _sample_init(self, graph):
         self.postprocessor.fit(self)
         graph=self.postprocessor.postprocess(graph)
@@ -26,6 +24,20 @@ class MoleculeSampler(UberSampler):
         return graph._score
 
 
+class PostProcessor:
+    def __init__(self):
+        pass
+
+    def fit(self, other):
+        self.vectorizer=other.vectorizer
+
+    def postprocess(self, graph):
+        grmgr=GraphManager(graph,self.vectorizer)
+        graph=grmgr.get_base_graph()
+        graph.graphmanager=grmgr
+        return graph
+
+
 
 class GraphManager(object):
     '''
@@ -33,31 +45,10 @@ class GraphManager(object):
     def get_estimateable(self):
     def get_base_graph(self):
     def get_abstract_graph(self):
-
     '''
-    def __init__(self,sequence_name,sequence,vectorizer,structure):
-        self.sequence_name=sequence_name
-        self.sequence=sequence
-        self.vectorizer=vectorizer
-        self.structure=structure
-
-        # create a base_graph , expand
-        base_graph=conv.sequence_dotbracket_to_graph(seq_info=sequence, seq_struct=structure)
-        self.base_graph=vectorizer._edge_to_vertex_transform(base_graph)
-
-        # get an expanded abstract graph
-        abstract_graph=forgi.get_abstr_graph(structure)
-        abstract_graph=vectorizer._edge_to_vertex_transform(abstract_graph)
-
-        #connect edges to nodes in the abstract graph
-        self.abstract_graph=edge_parent_finder(abstract_graph,self.base_graph)
-
-
-        # we are forced to set a label .. for eden reasons
-        def name_edges(graph,what=''):
-            for n,d in graph.nodes(data=True):
-                if 'edge' in d:
-                    d['label']=what
+    def __init__(self,graph,vectorizer):
+        self.base_graph=vectorizer._edge_to_vertex_transform(graph)
+        self.abstract_graph=make_abstract(self.base_graph.copy())
 
         # in the abstract graph , all the edge nodes need to have a contracted attribute.
         # originaly this happens naturally but since we make multiloops into one loop there are some left out
@@ -65,36 +56,16 @@ class GraphManager(object):
             for n,d in graph.nodes(data=True):
                 if 'contracted' not in d:
                     d['contracted']=set()
-
-        name_edges(self.abstract_graph)
         setset(self.abstract_graph)
 
-
     def get_estimateable(self):
-
         # returns an expanded, undirected graph
         # that the eden machine learning can compute
         return nx.disjoint_union(self.base_graph,self.abstract_graph)
-
     def get_base_graph(self):
-        if 'directed_base_graph' not in self.__dict__:
-            self.directed_base_graph= graphlearn.abstract_graphs.rnaabstract.expanded_rna_graph_to_digraph(self.base_graph)
-
-        return self.directed_base_graph
-
+        return self.base_graph
     def get_abstract_graph(self):
         return self.abstract_graph
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -149,7 +120,14 @@ def make_abstract(extgraph):
     '''
     for n,d in abstract_graph.nodes(data=True):
         #look at all the children and their neighbors parents
-        d['label']= len(d['contracted'])
+
+
+        if len(d['contracted'])>1:
+            d['label']= str( len(d['contracted']) )
+        else:
+            d['label']= extgraph.node[ f(d['contracted']) ]['label']
+
+
         if len(d['contracted']) ==1 and 'edge' in extgraph.node [ f(d['contracted'])]:
             d['edge']=True
             d['label']='edge'
