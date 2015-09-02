b222f7c62e019cbdc90b62111163538d750eabe1
Fabrizio Costa <xfcosta@gmail.com>
2015-09-02 15:57:52 +0200

    cleaning

--- graphlearn/abstract_graphs/graphmanager.py
+++ graphlearn/abstract_graphs/graphmanager.py
@@ -2,7 +2,7 @@
 ubersamplers fit will take a list of graphmanager as input.
 '''
 
-import subprocess  as sp
+import subprocess as sp
 import eden.converter.rna as conv
 import forgi
 import networkx as nx
@@ -14,28 +14,30 @@ from eden.graph import Vectorizer
 import rnasampler as rna
 import rnaabstract as rnaa
 
-def fromfasta(file='RF00005.fa',vectorizer=None):
-    s=[]
-    with open('RF00005.fa') as f:
+
+def fromfasta(fname=None, vectorizer=None):
+    s = []
+    with open(fname) as f:
         for line in f:
             s.append(line)
 
     while s:
-        seqname=s[0]
-        seq=''
-        for i,l in enumerate(s[1:]):
+        seqname = s[0]
+        seq = ''
+        for i, l in enumerate(s[1:]):
             if l[0] != '>':
-                seq+=l.strip()
+                seq += l.strip()
             else:
                 break
 
-        shape=callRNAshapes(seq)
+        shape = callRNAshapes(seq)
         if shape:
-            yield GraphManager(seqname,seq,vectorizer,shape)
-        s=s[i+1:]
+            yield GraphManager(seqname, seq, vectorizer, shape)
+        s = s[i + 1:]
 
 
 class GraphManager(object):
+
     '''
     these are the basis for creating a fitting an ubersampler
     def get_estimateable(self):
@@ -43,88 +45,91 @@ class GraphManager(object):
     def get_abstract_graph(self):
 
     '''
-    def __init__(self,sequence_name,sequence,vectorizer,structure):
-        self.sequence_name=sequence_name
-        self.sequence=sequence
-        self.vectorizer=vectorizer
-        self.structure=structure
+
+    def __init__(self, sequence_name, sequence, vectorizer, structure):
+        self.sequence_name = sequence_name
+        self.sequence = sequence
+        self.vectorizer = vectorizer
+        self.structure = structure
 
         # create a base_graph , expand
-        base_graph=conv.sequence_dotbracket_to_graph(seq_info=sequence, seq_struct=structure)
-        self.base_graph=vectorizer._edge_to_vertex_transform(base_graph)
+        base_graph = conv.sequence_dotbracket_to_graph(seq_info=sequence, seq_struct=structure)
+        self.base_graph = vectorizer._edge_to_vertex_transform(base_graph)
 
         # get an expanded abstract graph
-        abstract_graph=forgi.get_abstr_graph(structure)
-        abstract_graph=vectorizer._edge_to_vertex_transform(abstract_graph)
+        abstract_graph = forgi.get_abstr_graph(structure)
+        abstract_graph = vectorizer._edge_to_vertex_transform(abstract_graph)
 
-        #connect edges to nodes in the abstract graph
-        self.abstract_graph=edge_parent_finder(abstract_graph,self.base_graph)
-
+        # connect edges to nodes in the abstract graph
+        self.abstract_graph = edge_parent_finder(abstract_graph, self.base_graph)
 
         # we are forced to set a label .. for eden reasons
-        def name_edges(graph,what=''):
-            for n,d in graph.nodes(data=True):
+        def name_edges(graph, what=''):
+            for n, d in graph.nodes(data=True):
                 if 'edge' in d:
-                    d['label']=what
+                    d['label'] = what
 
         # in the abstract graph , all the edge nodes need to have a contracted attribute.
         # originaly this happens naturally but since we make multiloops into one loop there are some left out
         def setset(graph):
-            for n,d in graph.nodes(data=True):
+            for n, d in graph.nodes(data=True):
                 if 'contracted' not in d:
-                    d['contracted']=set()
+                    d['contracted'] = set()
 
         name_edges(self.abstract_graph)
         setset(self.abstract_graph)
 
+    def graph(self):
+        # returns the nesting edges graph
+        pass
 
     def get_estimateable(self):
+        # disjoint_graph()
 
         # returns an expanded, undirected graph
         # that the eden machine learning can compute
-        return nx.disjoint_union(self.base_graph,self.abstract_graph)
+        return nx.disjoint_union(self.base_graph, self.abstract_graph)
 
     def get_base_graph(self):
+        # base_graph()
+
         if 'directed_base_graph' not in self.__dict__:
-            self.directed_base_graph= graphlearn.abstract_graphs.rnaabstract.expanded_rna_graph_to_digraph(self.base_graph)
+            self.directed_base_graph = graphlearn.abstract_graphs.rnaabstract.expanded_rna_graph_to_digraph(self.base_graph)
 
         return self.directed_base_graph
 
     def get_abstract_graph(self):
+        # abstract_graph
         return self.abstract_graph
-
-
-
-
 
 
 def callRNAshapes(sequence):
 
-    cmd='RNAshapes %s' % sequence
+    cmd = 'RNAshapes %s' % sequence
     out = sp.check_output(cmd, shell=True)
     s = out.strip().split('\n')
 
     for li in s[2:]:
-        #print li.split()
-        energy,shape,abstr= li.split()
-        if abstr=='[[][][]]':
+        # print li.split()
+        energy, shape, abstr = li.split()
+        if abstr == '[[][][]]':
             return shape
 
 
-def edge_parent_finder(abstract,graph):
+def edge_parent_finder(abstract, graph):
     # find out to which abstract node the edges belong
     # finding out where the edge-nodes belong, because the contractor cant possibly do this
     #draw.graphlearn_draw([abstract,graph],size=10, contract=False,vertex_label='id')
 
     getabstr = {contra: node for node, d in abstract.nodes(data=True) for contra in d.get('contracted', [])}
-    #print getabstr
+    # print getabstr
     for n, d in graph.nodes(data=True):
         if 'edge' in d:
             # if we have found an edge node...
             # lets see whos left and right of it:
-            zomg= graph.neighbors(n)
-            #print zomg
-            #draw.draw_center(graph,1,20,contract=False,size=20)
+            zomg = graph.neighbors(n)
+            # print zomg
+            # draw.draw_center(graph,1,20,contract=False,size=20)
             n1, n2 = zomg
 
             # case1: ok those belong to the same gang so we most likely also belong there.
@@ -137,7 +142,8 @@ def edge_parent_finder(abstract,graph):
 
                 # case 3: abstract intersect in radius 1 failed, so lets try radius 2
                 if not abstract_intersect:
-                    abstract_intersect = set(nx.single_source_shortest_path(abstract,getabstr[n1],2)) & set(nx.single_source_shortest_path(abstract,getabstr[n2],2))
+                    abstract_intersect = set(nx.single_source_shortest_path(abstract, getabstr[n1], 2)) & set(
+                        nx.single_source_shortest_path(abstract, getabstr[n2], 2))
                     if len(abstract_intersect) > 1:
                         print "weired abs intersect..."
 
@@ -146,6 +152,5 @@ def edge_parent_finder(abstract,graph):
                         abstract.node[ai_node]['contracted'].add(n)
                     else:
                         abstract.node[ai_node]['contracted'] = set([n])
-
 
     return abstract
--- graphlearn/abstract_graphs/molecules.py
+++ graphlearn/abstract_graphs/molecules.py
@@ -1,5 +1,5 @@
 import graphlearn.abstract_graphs.rnaabstract
-from ubergraphlearn import UberSampler,UberGrammar
+from ubergraphlearn import UberSampler, UberGrammar
 import ubergraphlearn
 import networkx as nx
 import graphlearn.utils.draw as draw
@@ -11,62 +11,65 @@ class MoleculeSampler(UberSampler):
 
     def _sample_init(self, graph):
         self.postprocessor.fit(self)
-        graph=self.postprocessor.postprocess(graph)
+        graph = self.postprocessor.postprocess(graph)
         self._score(graph)
         self._sample_notes = ''
         self._sample_path_score_set = set()
         return graph
 
-    def _score(self,graph):
-        estimateable=graph.graphmanager.get_estimateable()
-        super(MoleculeSampler,self)._score(estimateable)
-        graph._score=estimateable._score
+    def _score(self, graph):
+        estimateable = graph.graphmanager.get_estimateable()
+        super(MoleculeSampler, self)._score(estimateable)
+        graph._score = estimateable._score
         return graph._score
 
 
 class PostProcessor:
+
     def __init__(self):
         pass
 
     def fit(self, other):
-        self.vectorizer=other.vectorizer
+        self.vectorizer = other.vectorizer
 
     def postprocess(self, graph):
-        grmgr=GraphManager(graph,self.vectorizer)
-        graph=grmgr.get_base_graph()
-        graph.graphmanager=grmgr
+        grmgr = GraphManager(graph, self.vectorizer)
+        graph = grmgr.get_base_graph()
+        graph.graphmanager = grmgr
         return graph
 
 
-
 class GraphManager(object):
+
     '''
     these are the basis for creating a fitting an ubersampler
     def get_estimateable(self):
     def get_base_graph(self):
     def get_abstract_graph(self):
     '''
-    def __init__(self,graph,vectorizer):
-        self.base_graph=vectorizer._edge_to_vertex_transform(graph)
-        self.abstract_graph=make_abstract(self.base_graph.copy())
+
+    def __init__(self, graph, vectorizer):
+        self.base_graph = vectorizer._edge_to_vertex_transform(graph)
+        self.abstract_graph = make_abstract(self.base_graph.copy())
 
         # in the abstract graph , all the edge nodes need to have a contracted attribute.
         # originaly this happens naturally but since we make multiloops into one loop there are some left out
         def setset(graph):
-            for n,d in graph.nodes(data=True):
+            for n, d in graph.nodes(data=True):
                 if 'contracted' not in d:
-                    d['contracted']=set()
+                    d['contracted'] = set()
         setset(self.abstract_graph)
 
     def get_estimateable(self):
         # returns an expanded, undirected graph
         # that the eden machine learning can compute
-        return nx.disjoint_union(self.base_graph,self.abstract_graph)
+        return nx.disjoint_union(self.base_graph, self.abstract_graph)
+
     def get_base_graph(self):
         return self.base_graph
+
     def get_abstract_graph(self):
         return self.abstract_graph
-
 
 
 '''
@@ -88,93 +91,88 @@ def make_abstract(extgraph):
     '''
 
     # annotate base graph
-    for n,d in extgraph.nodes(data=True):
-        d['cycle']=list(node_to_cycle(extgraph,n))
+    for n, d in extgraph.nodes(data=True):
+        d['cycle'] = list(node_to_cycle(extgraph, n))
         d['cycle'].sort()
 
     # prepare
-    abstract_graph=nx.Graph()
-    def fhash(stuff):
-        return eden.fast_hash(stuff,2**20-1)
+    abstract_graph = nx.Graph()
 
+    def fhash(stuff):
+        return eden.fast_hash(stuff, 2 ** 20 - 1)
 
     # make sure most of the abstract nodes are created.
     # base graph nodes have a list of abstract parents.
-    for n,d in extgraph.nodes(data=True):
+    for n, d in extgraph.nodes(data=True):
         # make sure abstract node exists
         cyclash = fhash(d['cycle'])
 
         if cyclash not in abstract_graph.node:
             abstract_graph.add_node(cyclash)
-            abstract_graph.node[cyclash]['contracted']= set(d['cycle'])
+            abstract_graph.node[cyclash]['contracted'] = set(d['cycle'])
 
         # tell everyone interested about it
         for e in d['cycle']:
-            node=extgraph.node[e]
+            node = extgraph.node[e]
             if 'parent' not in node:
                 node['parent'] = set()
             node['parent'].add(cyclash)
 
-    #connect nodes in the abstract graph
-    f=lambda x: list(x)[0]
-    for n,d in abstract_graph.nodes(data=True):
-        #look at all the children and their neighbors parents
+    # connect nodes in the abstract graph
+    f = lambda x: list(x)[0]
+    for n, d in abstract_graph.nodes(data=True):
+        # look at all the children and their neighbors parents
 
-
-        if len(d['contracted'])>1:
+        if len(d['contracted']) > 1:
             # setting label for cycles..
 
             # this will only use the length..
             #d['label']= "cycle "+str( len(d['contracted']) )
             # but i might as well use the hash of labels of all the contracted nodes
 
-
-            labels= [   ord( extgraph.node[childid]['label']) for childid in d['contracted']   ]
+            labels = [ord(extgraph.node[childid]['label']) for childid in d['contracted']]
             labels.sort()
-            d['label']="cycle %d" % fhash(labels)
-
+            d['label'] = "cycle %d" % fhash(labels)
 
         else:
-            d['label']= extgraph.node[ f(d['contracted']) ]['label']
+            d['label'] = extgraph.node[f(d['contracted'])]['label']
 
-
-        if len(d['contracted']) ==1 and 'edge' in extgraph.node [ f(d['contracted'])]:
-            d['edge']=True
-            d['label']=d['label']
+        if len(d['contracted']) == 1 and 'edge' in extgraph.node[f(d['contracted'])]:
+            d['edge'] = True
+            d['label'] = d['label']
         # for all nodes
         for base_node in d['contracted']:
             base_neighbors = extgraph.neighbors(base_node)
             # for all the neighbors
             for neigh in base_neighbors:
                 # find out if we have to build a connector node
-                if len (extgraph.node[neigh]['cycle']) > 1 and len (d['contracted']) > 1:
+                if len(extgraph.node[neigh]['cycle']) > 1 and len(d['contracted']) > 1:
 
                     for other in extgraph.node[neigh]['parent']:
                         if other != n:
-                            l=[other,n]
+                            l = [other, n]
                             l.sort()
                             connector = fhash(l)
                             if connector not in abstract_graph.node:
                                 # we need to consider making the edge the actual intersect of the two...
 
                                 abstract_graph.add_node(connector)
-                                abstract_graph.node[connector]['edge']=True
+                                abstract_graph.node[connector]['edge'] = True
 
-                                #abstract_graph.node[connector]['label']='edge'
+                                # abstract_graph.node[connector]['label']='edge'
                                 num_shared_nodes = len(abstract_graph.node[other]['contracted'] & d['contracted'])
-                                abstract_graph.node[connector]['label']="shared"+str(num_shared_nodes)
+                                abstract_graph.node[connector]['label'] = "shared" + str(num_shared_nodes)
 
-                                abstract_graph.add_edge(other,connector)
-                                abstract_graph.add_edge(connector,n)
+                                abstract_graph.add_edge(other, connector)
+                                abstract_graph.add_edge(connector, n)
 
                 else:
                     for e in extgraph.node[neigh]['parent']:
-                        abstract_graph.add_edge(n,e)
+                        abstract_graph.add_edge(n, e)
     return abstract_graph
 
 
-
-def node_to_cycle(graph,n,min_cycle_size=3):
+def node_to_cycle(graph, n, min_cycle_size=3):
     """
     :param graph:
     :param n: start node
@@ -191,11 +189,11 @@ def node_to_cycle(graph,n,min_cycle_size=3):
         - it is also possible that the newly found cycle doesnt contain our start node. so we check for that
     """
 
-    def close_cycle(collisions,parent,root):
+    def close_cycle(collisions, parent, root):
         '''
             we found a cycle, but that does not say that the root node is part of that cycle..S
         '''
-        def extend_path_to_root(work_list,parent_dict,root):
+        def extend_path_to_root(work_list, parent_dict, root):
             """
             :param work_list: list with start node
             :param parent_dict: the tree like dictionary that contains each nodes parent(s)
@@ -205,86 +203,85 @@ def node_to_cycle(graph,n,min_cycle_size=3):
              --- mm we dont care if we get the shortest path.. that is true for cycle checking.. but may be a problem in
              --- cycle finding.. dousurururururu?
             """
-            current=work_list[-1]
+            current = work_list[-1]
             while current != root:
-                work_list.append(  parent_dict[current][0]  )
-                current=work_list[-1]
+                work_list.append(parent_dict[current][0])
+                current = work_list[-1]
             return work_list[:-1]
 
         # any should be fine. e is closing a cycle,
         # note: e might contain more than one hit but we dont care
-        e= collisions.pop()
-        #print 'r',e
+        e = collisions.pop()
+        # print 'r',e
         # we closed a cycle on e so e has 2 parents...
-        li=parent[e]
+        li = parent[e]
         a = [li[0]]
         b = [li[1]]
-        #print 'pre',a,b
+        # print 'pre',a,b
         # get the path until the root node
-        a=extend_path_to_root(a,parent,root)
-        b=extend_path_to_root(b,parent,root)
-        #print 'comp',a,b
-        #of the paths to the root node dont overlap, the root node musst be in the loop
-        a=set(a)
-        b=set(b)
-        intersect=a & b
-        if len(intersect) ==0:
-            paths= a | b
+        a = extend_path_to_root(a, parent, root)
+        b = extend_path_to_root(b, parent, root)
+        # print 'comp',a,b
+        # of the paths to the root node dont overlap, the root node musst be in the loop
+        a = set(a)
+        b = set(b)
+        intersect = a & b
+        if len(intersect) == 0:
+            paths = a | b
             paths.add(e)
             paths.add(root)
-            return  paths
+            return paths
         return False
 
-
     FAILEDVALUE = set([n])
-    frontier= set([n])
-    step=0
-    visited=set()
-    parent=defaultdict(list)
+    frontier = set([n])
+    step = 0
+    visited = set()
+    parent = defaultdict(list)
 
     while frontier:
-        #print frontier
-        step+=1
+        # print frontier
+        step += 1
 
         # give me new nodes:
-        next=[]
+        next = []
         for front_node in frontier:
             new = set(graph.neighbors(front_node)) - visited
-            next.append(  new )
+            next.append(new)
             for e in new:
                 parent[e].append(front_node)
 
         # we merge the new nodes.   if 2 sets collide, we found a cycle of even length
-        while len(next)>1:
+        while len(next) > 1:
             # merge
-            s1=next[1]
-            s2=next[0]
+            s1 = next[1]
+            s2 = next[0]
             merge = s1 | s2
 
             # check if we havee a cycle   => s1,s2 overlap
-            if  len(merge) < len(s1)+len(s2):
-                col= s1&s2
-                cycle=close_cycle(col,parent,n)
+            if len(merge) < len(s1) + len(s2):
+                col = s1 & s2
+                cycle = close_cycle(col, parent, n)
                 if cycle:
-                    if step*2 > min_cycle_size:
+                    if step * 2 > min_cycle_size:
                         return cycle
                     return FAILEDVALUE
 
-            #delete from list
-            next[0]=merge
+            # delete from list
+            next[0] = merge
             del next[1]
-        next=next[0]
+        next = next[0]
 
         # now we need to check for cycles of uneven length => the new nodes hit the old frontier
         if len(next & frontier) > 0:
-            col= next&frontier
-            cycle = close_cycle(col,parent,n)
+            col = next & frontier
+            cycle = close_cycle(col, parent, n)
             if cycle:
-                if step*2-1 > min_cycle_size:
+                if step * 2 - 1 > min_cycle_size:
                     return cycle
                 return FAILEDVALUE
 
         # we know that the current frontier didntclose cycles so we dont need to look at them again
         visited = visited | frontier
-        frontier=next
+        frontier = next
     return FAILEDVALUE
