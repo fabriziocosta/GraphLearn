fbd45a27b0b8f7e2fc0c17133b0cafb0a49fde1a
smautner <qxc420@gmail.com>
2015-10-02 18:26:06 +0200

    normal sampling now works with preprocessor

--- graphlearn/graphtools.py
+++ graphlearn/graphtools.py
@@ -92,55 +92,6 @@ class AbstractGraphWrapper(object):
 
 
 
-class PreProcessor(object):
-
-    def fit(self, inputs,vectorizer):
-        self.vectorizer=vectorizer
-
-    def fit_transform(self,inputs,vectorizer):
-        '''
-
-        Parameters
-        ----------
-        input : many inputs
-
-        Returns
-        -------
-        graphwrapper iterator
-        '''
-        self.fit(inputs,vectorizer)
-        return self.transform(inputs)
-
-    def re_transform_single(self, graphwrapper):
-        '''
-
-        Parameters
-        ----------
-        graphwrapper
-
-        Returns
-        -------
-        a postprocessed graphwrapper
-        '''
-        # mabe a copy?
-        return graphwrapper
-
-    def transform(self,inputs):
-        '''
-
-        Parameters
-        ----------
-        inputs : list of things
-
-        Returns
-        -------
-        graphwrapper : iterator
-        '''
-        for i in input:
-            yield GraphWrapper(self.vectorizer._edge_to_vertex_transform(i),self.vectorizer)
-
-
-
 
 
 class GraphWrapper(AbstractGraphWrapper):
