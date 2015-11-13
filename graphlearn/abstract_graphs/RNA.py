c9e51eff869a234c5eb4b2a54ff93d59d0c8271e
smautner <qxc420@gmail.com>
2015-11-13 01:29:56 +0100

    path to cmsearch not hardcoded any longer

--- graphlearn/abstract_graphs/RNA.py
+++ graphlearn/abstract_graphs/RNA.py
@@ -514,7 +514,7 @@ class AbstractSampler(Sampler):
         super(self.__class__,self)._sample_path_append(graph,force=force)
 
 
-def infernal_checker(sequence_list,cmfile='rf00005.cm'):
+def infernal_checker(sequence_list,cmfile='rf00005.cm', cmsearchbinarypath='../toolsdata/cmsearch'):
     '''
     :param sequences: a bunch of rna sequences
     :return: get evaluation from cmsearch
@@ -522,7 +522,7 @@ def infernal_checker(sequence_list,cmfile='rf00005.cm'):
     write_fasta(sequence_list,filename='temp.fa')
     sequence_list = [ s for s in sequence_list if is_sequence(s.replace('F',''))  ]
     #print sequence_list
-    return call_cm_search(cmfile,'temp.fa',len(sequence_list))
+    return call_cm_search(cmfile,'temp.fa',len(sequence_list),cmsearchbinarypath)
 
 
 
@@ -547,9 +547,9 @@ def write_fasta(sequences,filename='asdasd'):
         f.write(fasta)
 
 
-def call_cm_search(cmfile,filename, count):
+def call_cm_search(cmfile,filename, count,cmsearchbinpath):
 
-    out = sp.check_output('../cmsearch -g --noali --incT 0  %s %s' %(cmfile, filename), shell=True)
+    out = sp.check_output('%s -g --noali --incT 0  %s %s' %(cmsearchbinpath,cmfile, filename), shell=True)
     # -g global
     # --noali, we dont want to see the alignment, score is enough
     # --incT 0 we want to see everything with score > 0
