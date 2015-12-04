dfafa3187ebff91e72cae1e13416a2d79d79973a
smautner <qxc420@gmail.com>
2015-12-04 20:54:00 +0100

    typo

--- graphlearn/utils/evaltools.py
+++ graphlearn/utils/evaltools.py
@@ -2,7 +2,7 @@ import subprocess as sp
 
 def dotbracket_to_shape(dbstring,shapesversion=2):
     
-    if shapesversion=3:
+    if shapesversion==3:
         cmd = 'RNAshapes --mode abstract --shapeLevel 3 "%s"' % dbstring
     else:
         cmd = 'RNAshapes -D "%s" -t 3 ' % dbstring
