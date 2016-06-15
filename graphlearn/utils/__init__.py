478b6f835ed02ca7cd4689920dd3d09d856603b6
smautner <mautnerstef@gmail.com>
2016-06-15 20:29:28 +0200

    added chart draw function to utils.init

--- graphlearn/utils/__init__.py
+++ graphlearn/utils/__init__.py
@@ -8,3 +8,30 @@ def selectdraw(graphs,chem=False,**args):
     else:
         import draw
         draw.graphlearn(graphs,**args)
+
+
+
+
+# this is copy pasted in EVERY notebook. now its here 
+import matplotlib.pyplot as plt
+def plot_scores(scoreslist='list of lists', labels=[]):
+
+    if len(labels)==0:
+        labels= ['index: %d' % i for i in len(scoreslist)]
+
+    plt.figure(figsize=(10,5))
+
+    for j,scores in enumerate(scoreslist):
+        plt.plot(scores, label=labels[j])
+
+    maa= max([ max(l) for l in scoreslist  ])
+    mii= min([min(l) for l in scoreslist])
+    plt.legend(loc='lower right')
+    plt.grid()
+    plt.ylim(mii*1.1,maa*1.1)
+    plt.show()
+
+
+# i might need this one day
+def chunk(l,n):
+    return [l[i:i+n] for i in xrange(0, len(l), n)]
