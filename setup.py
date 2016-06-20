ca0edc7f9392335f3faa3108bc1e7d4be7660f81
smautner <mautnerstef@gmail.com>
2016-06-20 15:06:57 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graphlearn',
+      version='0.0.1',
+      description='Generate graphs, given examples.',
+      url='http://github.com/smautner/GraphLearn',
+      author='Stefan Mautner',
+      author_email='stefmautner@gmeeru.com',
+      license='YOUAREATHRIEF',
+      packages=['greaphlearn'],
+      zip_safe=False)
