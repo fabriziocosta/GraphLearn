c8ac0580deb90d40f2553919449179915d74c5ea
smautner <smautner@>
2016-08-22 09:29:07 +0200

    layered drawing v2 is here. is not using the vectorized-nesting-graph but graphs w/o decomposer

--- graphlearn/utils/draw.py
+++ graphlearn/utils/draw.py
@@ -286,7 +286,7 @@ def graphlearn_layered2(graphs,**args):
 
     '''
 
-    '''
+
     def calc_avg_position(nodelist,posdict):
         #print 'calc avg pos'
         if len(nodelist)==0:
@@ -296,87 +296,49 @@ def graphlearn_layered2(graphs,**args):
         ypos= sum([ posdict[i][1] for i in nodelist ])/len(nodelist)
         return (xpos,ypos)
 
-    def get_leafes(graph,node):
-        #print 'get leafes %d' % node
-        if graph.node[node].get('contracted',0) == 0:
-            return [node]
-        nodes=[node]
-        leafes=[]
-        while len(nodes)>0:
-            current= nodes.pop()
-            # contraction also includes edge nodes -> ignore those
-            if current not in graph.nodes():
-                continue
-            if 'contracted' in graph.node[current]:
-                children=list(  graph.node[current]['contracted']  )
-                nodes += children
-            else:
-                leafes.append(current)
-
-        #print leafes
-        return leafes
-    '''
 
     finished_graphs=[]
     poslist=[]
+    maxlayers=0
     for graph in graphs:
-        # how many layers are there? also make a list of nodes for each layer
-        '''
-        nodelayer=defaultdict(list)
-        layercount=-1
-        for n,d in graph.nodes(data=True):
-            layer = d.get('layer', -1)
-            layercount = max(layercount,layer)
-            nodelayer[d['layer']].append(n)
-        if layercount == -1:
-            print "layer annotation missing in graph"
-            break
-        '''
 
-        # layout layer 0
-        pos = nx.graphviz_layout(graph, prog='neato', args="-Gmode=KK")
+        # make a list of all the graphs
+        layered_graphs=[graph]
+        while 'original' in graph.graph:
+            layered_graphs.append(graph.graph['original'])
+            graph=graph.graph['original']
+        maxlayers=max(maxlayers, len(layered_graphs))
+        # make the layout for the biggest one :)
+        pos = nx.graphviz_layout(layered_graphs[-1], prog='neato', args="-Gmode=KK")
+
 
         # pos attribute loks like this:
         #pos = {i: (rna_object.get(i).X, rna_object.get(i).Y)
         #           for i in range(len(graph.graph['structure']))}
 
-
-
-        for layerid in range(1,layercount+1):
-
+        for i in range( len(layered_graphs)-2,-1,-1):
             new_positions={}
-            # nodes in the layer:
-            nodes = nodelayer[layerid]
-            for node in nodes:
-                nulllayernodes = get_leafes(graph,node)
-                new_positions[node]=calc_avg_position(nulllayernodes,pos)
+
+            for node in layered_graphs[i].nodes():
+                new_positions[node]=calc_avg_position(layered_graphs[i].node[node].get('contracted',set()),pos)
 
             # move all the nodes by such and such
             # nodes in prev layer:
-            moveby_x = max( pos[i][0] for i in nodelayer[layerid-1] ) + 100
-            #moveby_y = max( pos[i][1] for i in nodelayer[layerid-1] ) - 30
-            moveby_y = ((-1)**layerid) * 30
+            moveby_x = max( [ pos[n][0] for n in layered_graphs[i+1].nodes()]  ) + 100
+            moveby_y = ((-1)**i) * 30
             for k,v in new_positions.items():
                 new_positions[k] = (v[0]+moveby_x,v[1]+moveby_y)
-
             pos.update(new_positions)
 
-
-
-
-        # color dark edged:
-        if False:
-            for node,d in graph.nodes(data=True):
-                if 'contracted' in d:
-                    for other in d['contracted']:
-                        if other in graph.nodes():
-                            graph[node][other]['dark_edge_color']=d['layer']
-
-        finished_graphs.append(graph)
+        g=nx.union_all(layered_graphs)
+        for n,d in g.nodes(data=True):
+            for n2 in d.get('contracted',[]):
+                g.add_edge(n,n2,nesting=True,label='')
+        finished_graphs.append(g)
         poslist.append(pos)
 
     # draw
-    args['size_x_to_y_ratio']= layercount+1
+    args['size_x_to_y_ratio']= maxlayers
     args['pos']= poslist
     args['dark_edge_color']= 'dark_edge_color'
     graphlearn(finished_graphs,**args)
