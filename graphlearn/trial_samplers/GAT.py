d8b794146dad3bb437ca1d026a3e10b38689b8a3
S Mautner <mautner@cs.uni-freiburg.de>
2017-02-15 20:28:38 +0100

    hayaku!

--- graphlearn/trial_samplers/GAT.py
+++ graphlearn/trial_samplers/GAT.py
@@ -104,6 +104,9 @@ def generative_adersarial_training(sampler, n_iterations= 3, seedgraphs= None, p
 
     # prepare 2 class svm
     sampler.estimatorobject = TwoClass(cv=5,recalibrate=True)
+
+
+
     new_decomposers = sampler.fit_make_decomposers(constructed_graphs[-1])
     if partial_estimator:
         sampler.estimatorobject._partial(sampler.decomps_to_vectors(seed_decomposers),sampler.decomps_to_vectors(new_decomposers))
@@ -142,3 +145,140 @@ def generative_adersarial_training(sampler, n_iterations= 3, seedgraphs= None, p
         print '%d ' % i,
 
     return estimators, constructed_graphs #, seed_vectors
+
+
+
+
+
+
+
+
+
+
+
+
+#######################################################
+########################################################
+# this is the hack version
+
+
+def generative_adersarial_training_HACK(sampler, n_iterations= 3, seedgraphs= None,neg_vectors=None, partial_estimator=True):
+
+    # fit initial 1 class svm
+    seed_decomposers = sampler.fit_make_decomposers(seedgraphs)
+    seed_vectors = sampler.decomps_to_vectors(seed_decomposers)
+
+    #neg_decomposers = sampler.fit_make_decomposers(negatives)
+    #neg_vectors = sampler.decomps_to_vectors(neg_decomposers)
+
+
+    sampler.fit_grammar(seed_decomposers)
+    #sampler.fit_estimator(seed_decomposers)
+
+
+    # prepare 2 class svm
+    sampler.estimatorobject = TwoClass(cv=5,recalibrate=True)
+    sampler.estimatorobject.fit(seed_vectors,neg_vectors)
+
+
+    # save constructed graphs...
+    constructed_graphs = []
+    constructed_vectors = []
+    constructed_graphs.append(flatten(sampler.transform(seedgraphs)))
+    constructed_vectors.append(graphs_to_vectors(sampler,constructed_graphs[-1]))
+
+
+    # save estimator
+    estimators=[]
+    estimators.append(copy.deepcopy(sampler.estimatorobject.cal_estimator))
+
+
+    new_decomposers = sampler.fit_make_decomposers(constructed_graphs[-1])
+    if partial_estimator:
+        sampler.estimatorobject._partial(sampler.decomps_to_vectors(seed_decomposers),sampler.decomps_to_vectors(new_decomposers))
+    else:
+        sampler.fit_estimator(seed_decomposers, new_decomposers)
+
+    def make_partial_fit_args(graphs):
+        # return X, y
+        # with old and new
+        vectors= sampler.vectorizer.transform(map( lambda x: x.pre_vectorizer_graph(),sampler.fit_make_decomposers(graphs)))
+        X= vstack((vectors, seed_vectors))
+        return  X, [-1]*len(graphs)+[1]*len(seedgraphs)
+
+    for i in range(n_iterations):
+        # construct
+        constructed_graphs.append(flatten(sampler.transform(seedgraphs)))
+        if len(constructed_graphs[-1]) == 0:
+            print "NO GRAPHZ GENERATED,, eden will die soon"
+        constructed_vectors.append(graphs_to_vectors(sampler,constructed_graphs[-1]))
+
+        # partial fit:
+        #a,b=make_partial_fit_negs(constructed_graphs[-1])
+        #sampler.estimatorobject.cal_estimator.partial_fit(a,b, sample_weight=[1]*len(constructed_graphs[-1]))
+        # new fit
+        if partial_estimator:
+            X,y =make_partial_fit_args(constructed_graphs[-1])
+            sampler.estimatorobject.cal_estimator.partial_fit(X,y)
+        else:
+            weights=get_sample_weights(seedgraphs,constructed_graphs)
+            #print len(weights),seed_vectors.shape, vstack(constructed_vectors).shape, len(flatten(constructed_graphs))
+            #sampler.estimatorobject.fit(seed_vectors,vstack(constructed_vectors[1:]),sample_weight=weights)
+            sampler.estimatorobject.fit(seed_vectors,vstack(constructed_vectors),sample_weight=weights)
+
+        # save esti
+        estimators.append(copy.deepcopy(sampler.estimatorobject.cal_estimator))
+        print '%d ' % i,
+
+    return estimators, constructed_graphs #, seed_vectors
+
+
+################################################################
+################################################################
+# THIS IS THE PROPPER VERSION THAT WILL BE CREATED LATER>>>
+def generative_adersarial_training_realneg(sampler, n_iterations= 3, seedgraphs= None,negatives=None, partial_estimator=True):
+
+
+    # fit grammar, make vectors
+    seed_vectors = sampler.decomps_to_vectors(seed_decomposers)
+    seed_decomposers = sampler.fit_make_decomposers(seedgraphs)
+    sampler.fit_grammar(seed_decomposers)
+
+    # save results
+    constructed_graphs = []
+    constructed_vectors = []
+    estimators=[]
+
+    sampler.estimatorobject = TwoClass(cv=5,recalibrate=True)
+
+
+
+
+    for i in range(n_iterations):
+        #new_decomposers = sampler.fit_make_decomposers(constructed_graphs[-1])
+
+
+
+        # fitting the estimator object
+        # sampler.fit_estimator(seed_decomposers, new_decomposers)
+
+        weights=get_sample_weights(seedgraphs,constructed_graphs)
+        sampler.estimatorobject.fit(seed_vectors,vstack(constructed_vectors),sample_weight=weights)
+
+
+        # make graphs
+        constructed_graphs.append(flatten(sampler.transform(seedgraphs)))
+        if len(constructed_graphs[-1]) == 0:
+            print "NO GRAPHZ GENERATED,, eden will die soon"
+        constructed_vectors.append(graphs_to_vectors(sampler,constructed_graphs[-1]))
+
+
+
+
+        # save esti
+        estimators.append(copy.deepcopy(sampler.estimatorobject.cal_estimator))
+        print '%d ' % i,
+
+    return estimators, constructed_graphs #, seed_vectors
+
+
