904640948c12c573ca55b460e1b813c17170ee48
S Mautner <mautner@cs.uni-freiburg.de>
2019-11-28 11:57:31 +0100

    lsgg_life now working..

--- graphlearn/lsgg_life.py
+++ graphlearn/lsgg_life.py
@@ -1,32 +1,30 @@
-
-
-
-
-
 from graphlearn import lsgg 
+import structout as so
 from graphlearn import lsgg_cip
 import networkx as nx
-
+import numpy as np
+import random
 # lsgg_life -> Loose InterFacE around the actual interface
+# mmm lscc LOCO loose context is also ok... 
 
 
 class LIFE(lsgg.lsgg):
 
     def _extract_core_and_interface(self, **kwargs):
         return extract_core_and_interface(thickness_life=self.decomposition_args['thickness_life'] , **kwargs)
-
     
     def _congruent_cips(self, cip):
         cips = self.productions.get(cip.interface_hash, {}).values()
-        cips_ = [(cip_,cip_.life_vector*cip.life_vector) for cip_ in cips if cip_.core_hash != cip.core_hash]
+        
+        def dist(a,b):
+            if type(a)==np.ndarray and type(b)==np.ndarray:
+                return np.dot(a,b.T)[0][0]
+            else: 
+                return 0
+        cips_ = [(cip_,dist(cip_.life_vector,cip.life_vector)) for cip_ in cips if cip_.core_hash != cip.core_hash]
 
-	
-        print(cips_)
         random.shuffle(cips_)
-        return cips_ 
-
-
-
+        return [ c for c,i in  cips_ ]
 
 def extract_core_and_interface(root_node=None,
                                graph=None,
@@ -39,13 +37,13 @@ def extract_core_and_interface(root_node=None,
     lsgg_cip._add_hlabel(graph)
     dist = {a:b for (a,b) in lsgg_cip.short_paths(graph,
                                          root_node if isinstance(root_node,list) else [root_node],
-                                         thickness+radius)}
+                                         thickness+radius+thickness_life)}
 
     core_nodes = [id for id, dst in dist.items() if dst <= radius]
     interface_nodes = [id for id, dst in dist.items()
                        if radius < dst <= radius + thickness]
 
-    normal_cip =  _finalize_cip(root_node,graph,radius,thickness,dist,core_nodes,interface_nodes)
+    normal_cip =  lsgg_cip._finalize_cip(root_node,graph,radius,thickness,dist,core_nodes,interface_nodes)
 
 
     # NOW COMES THE LIFE PART
@@ -54,7 +52,11 @@ def extract_core_and_interface(root_node=None,
                        if (radius+thickness) < dst <= (radius + thickness+ thickness_life)]
 
     life_graph = graph.subgraph(life_nodes) 
-    normal_cip.life_vector = lsgg_cip.eg.vectorize(nx.Graph(life_graph))
+    
+    loosecontext = nx.Graph(life_graph)
+    #so.gprint(loosecontext)
+    nn = loosecontext.number_of_nodes() > 2
+    normal_cip.life_vector = lsgg_cip.eg.vectorize([loosecontext])[0].toarray() if nn else None
 
     return normal_cip
 
--- graphlearn/test_life.py
+++ graphlearn/test_life.py
@@ -3,8 +3,9 @@ import lsgg_life
 import json 
 import networkx as nx 
 import structout as so
+import random
 def getgraphs():
-    with open("AID1224837.sdf.json", 'r') as handle:                                      
+    with open("chemtest.json", 'r') as handle:                                      
           stuff = json.loads(handle.read())
           def js_to_graph(x):
               g= nx.readwrite.node_link_graph(x)
@@ -17,15 +18,16 @@ def getgraphs():
 def test_life():
     g= getgraphs()
     grammar = lsgg_life.LIFE(  
-            decomposition_args={"radius_list": [0, 1],                
-                                "thickness_list": [1, 2],  
-                                "thickness_life": 2},
+            decomposition_args={"radius_list": [0,1], 
+                                "thickness_list": [1],  
+                                "thickness_life": 4},
             filter_args={"min_cip_count": 1,                               
                          "min_interface_count": 1}
             )
     #print(g[:10])
-    grammar.fit(g[:10])
-    grammar.neighbors(g[55])
+    grammar.fit(g[:100])
+    print("fitting done")
+    so.gprint(list(grammar.neighbors(g[55]))[:3])
 
 
 test_life()
