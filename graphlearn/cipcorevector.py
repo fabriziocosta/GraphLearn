f36fc2ac88676331aa8a901c75310ccbe9e8fa6a
S Mautner <mautner@cs.uni-freiburg.de>
2020-02-20 13:47:55 +0100

    cip core vector working with test oO

--- graphlearn/cipcorevector.py
+++ graphlearn/cipcorevector.py
@@ -1,33 +1,37 @@
 
 
-from graphlear.lsgg_core_interface_pair import * 
+from graphlearn.lsgg_core_interface_pair import * 
 from ego import real_vectorize as rv
+from graphlearn import LSGG
 
 
-class lsgg_extension:
+class LsggCoreVec(LSGG):
 
-    '''  attaches a vector for each core, representing the nodes contained,,, 
+    '''  attaches a vector for each CIP, representing the nodes contained,,, 
         i do this by attaching the vector to the cores when they are generated 
         (so i only need to compute the node vectorisation once)
         and transfer them to the CIPs when make_cip is called
     '''
+
+    def __init__(self,decomposer=None, **kwargs):
+        self.decomposer = decomposer
+        super(LsggCoreVec,self).__init__(**kwargs)
+
 
     ##########
     #  step one: cores get vectors attached
     #########
 
     def _get_cores(self, graph):
-        cores = [ core for core in lsgg_core_interface_pair.get_cores(graph, self.radii) if core]
+        cores= super(LsggCoreVec,self)._get_cores(graph)
         self.attach_vectors(cores, graph)
         return cores
-
 
 
     def attach_vectors(self, cores, graph):
         matrix = vertex_vec(graph, self.decomposer) # should put decomposer in init...
         for core in cores:
-            core.core_vec = self.make_core_vector(core, graph, node_vectors)
-
+            core.core_vec = self.make_core_vector(core, graph, matrix)
 
     def make_core_vector(self, core, graph, node_vectors): 
         c_set = set(core.nodes())
@@ -40,11 +44,13 @@ class lsgg_extension:
     ######
     
     def _make_cip(self, core=None, graph=None):
-        cip = super(lsgg_extension, self)._make_cip(core, graph) 
+        cip = super(LsggCoreVec, self)._make_cip(core, graph) 
         if cip:
             cip.core_vec= core.core_vec 
             return cip
-        
+    #####
+    # step 3: one should probably overwrite get_congruent cips to actually use the new vectors
+    ###########
 
 
 def vertex_vec(graph, decomposer, bitmask = 2**10-1): 
@@ -59,6 +65,5 @@ def vertex_vec(graph, decomposer, bitmask = 2**10-1):
     data_matrix = rv.to_sparse_matrix(encoding, node_ids, bitmask+2)
 
     return data_matrix
-
 
 
